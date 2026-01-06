acfc9b848f3bd67f572c7832b577643961a8a678
Harsh Aryan <aryan.harsh06@gmail.com>
2026-01-06 12:00:58 +0530

    intial commit

--- src/components/header-2.tsx
+++ src/components/header-2.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Button, buttonVariants } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
+import { cn } from '@/utils';
 import { MenuToggleIcon } from '@/components/menu-toggle-icon';
 import { useScroll } from '@/components/use-scroll';
 
